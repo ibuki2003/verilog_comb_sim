5ebdb281ef2714a972382c6dba8a995358d9d8c9
ibuki2003 <main@fuwa.dev>
2025-06-14 17:52:51 +0900

    build: setup tree-sitter

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  build: {
+    target: "esnext",
+    // polyfillDynamicImport: false,
+  },
 
   css: {
     preprocessorOptions: {
